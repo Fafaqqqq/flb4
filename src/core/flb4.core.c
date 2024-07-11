3033990a4fcb06008715c81330cb22d2ef7104f7
Yashenkov A.V. <aiashenkov@astralinux.ru>
2024-07-11 21:19:58 +0300

    Перенаправление пакетов на другой интерфейс

--- src/core/flb4.core.c
+++ src/core/flb4.core.c
@@ -1,12 +1,12 @@
 #include <linux/types.h>
 
-// #include "flb4_structs.h"
+#include "flb4_structs.h"
 #include "flb4_helper.h"
 
 // Заголовки bpf-функций
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
-// #include <bpf/bpf_tracing.h>
+#include <bpf/bpf_tracing.h>
 #include <linux/bpf.h>
 
 // Заголовки сетевого стека
@@ -14,53 +14,130 @@
 #include <linux/ip.h>
 #include <linux/in.h>
 #include <linux/tcp.h>
+#include <sys/socket.h>
 #include <sys/cdefs.h>
 
-#include "flb4_structs.h"
-#include "flb4_maps.h"
 
-static int real_idx = 0;
+#ifndef memcpy
+    #define memcpy __builtin_memcpy
+#endif
 
-__attribute__((__always_inline__))
-static inline int process_packet(void* data, void* data_end, __u64 offset) {
+#ifndef memset
+    #define memset __builtin_memset
+#endif
 
-	struct iphdr* ip = (struct iphdr*)((__u8*)data + offset);
-	// struct iphdr* ip = (__u8*)data + offset;
-	VALIDATE_HEADER(ip, data_end);
+// Константы - вынести в header
+#define IP_FRAGMENTED (0x3FFF)
 
-    struct vs_info* info = (struct vs_info*)NULL;
+#define SOURCE_IP  (0x0A8CE2F4)
+#define VIRTUAL_IP_ETH1 (0x0A8C00C9)
+#define VIRTUAL_IP_ETH2 (0x0A9600C8)
+#define REAL_IP  (0x0A960064)
 
-    __u32 vip = bpf_ntohl(ip->daddr);
-
-    info = (struct vs_info*)(bpf_map_lookup_elem(&vs_map, &vip));
-
-    if (!info) {
-        return XDP_PASS;
-    }
-
-    if (info->change_src_ip) {
-        ip->saddr = bpf_htons(vip);
-    }
-
-    __u8   real_count_idx = 0;
-    __u32* real_count     = (__u32*)(bpf_map_lookup_elem(&real_servers_count, &real_count_idx));
-
-    if (!real_count) {
-        return XDP_PASS;
-    }
-
-    __u32  real_server_idx = (real_idx++) % *real_count;
-    __u32* real_addr = (__u32*)(bpf_map_lookup_elem(&real_servers_map, &real_server_idx));
-
-    ip->daddr = bpf_htonl(*real_addr);
-
-    ip->check = 0;
-    ip->check = ipv4_csum(0, ip);
-
-	return XDP_TX;
+static __always_inline
+__u16 csum_fold_helper(__u32 csum) {
+    __u32 sum = csum;
+    while (sum >> 16)
+        sum = (sum & 0xffff) + (sum >> 16);
+    return ~sum;
 }
 
-SEC("xdp")
+static __always_inline
+__u16 ipv4_csum(__u32 csum, struct iphdr *iph) {
+    csum = bpf_csum_diff(0, 0, (void*)iph, sizeof(*iph), csum);
+    return csum_fold_helper(csum);
+}
+
+static __always_inline
+void update_tcp_checksum(struct tcphdr *tcph, __u32 old_saddr, __u32 new_saddr,
+                                              __u32 old_daddr, __u32 new_daddr) {
+    __u32 check = tcph->check;
+
+    // Вычитаем старые значения псевдозаголовка
+    check = ~check;
+    check = bpf_csum_diff(&old_saddr, 4, &new_saddr, 4, check);
+    check = bpf_csum_diff(&old_daddr, 4, &new_daddr, 4, check);
+
+    // Добавляем новые значения псевдозаголовка
+    check = check;
+
+    tcph->check = csum_fold_helper(check);
+}
+
+__attribute__((__always_inline__))
+static inline int process_packet(struct xdp_md* ctx, void* data, void* data_end, __u64 offset) {
+
+	struct ethhdr* eth = (struct ethhdr*)data;
+	VALIDATE_HEADER(eth, data_end);
+
+	struct iphdr* ip = data + offset;
+	VALIDATE_HEADER(ip, data_end);
+
+
+    int action  = XDP_PASS;
+    int ifindex = 0;
+
+    __u32 old_saddr = ip->saddr;
+    __u32 old_daddr = ip->daddr;
+
+    if (ip->daddr == bpf_htonl(VIRTUAL_IP_ETH1) && ip->saddr != bpf_htonl(REAL_IP)) {
+		ip->daddr = bpf_htonl(REAL_IP);
+		ip->saddr = bpf_htonl(VIRTUAL_IP_ETH2);
+        ifindex  = 4;
+	}
+
+	if (ip->daddr == bpf_htonl(VIRTUAL_IP_ETH2) && ip->saddr == bpf_htonl(REAL_IP)) {
+		ip->saddr = bpf_htonl(VIRTUAL_IP_ETH1);
+		ip->daddr = bpf_htonl(SOURCE_IP);
+        ifindex = 3;
+	}
+
+    if (ifindex > 0) {
+        struct bpf_fib_lookup fib_attrs;
+
+        memset(&fib_attrs, 0, sizeof(fib_attrs));
+
+        fib_attrs.family   = AF_INET;
+        fib_attrs.ipv4_src = ip->saddr;
+        fib_attrs.ipv4_dst = ip->daddr;
+        fib_attrs.ifindex  = ifindex;
+
+        int fib_ret = bpf_fib_lookup(ctx, &fib_attrs, sizeof(fib_attrs), 0);
+
+        switch (fib_ret) {
+            case BPF_FIB_LKUP_RET_SUCCESS:         /* lookup successful */
+                memcpy(eth->h_dest, fib_attrs.dmac, ETH_ALEN);
+                memcpy(eth->h_source, fib_attrs.smac, ETH_ALEN);
+
+                // Пересчитываем контрольную сумму IP-заголовка
+                ip->check = 0;
+                ip->check = ipv4_csum(0, ip);
+
+                bpf_printk("protocol: %d", ip->protocol);
+
+                if (ip->protocol == IPPROTO_TCP) {
+                    struct tcphdr* tcph = (struct tcphdr* )(ip + 1);
+                    VALIDATE_HEADER(tcph, data_end);
+
+                    update_tcp_checksum(tcph, old_saddr, ip->saddr, old_daddr, ip->daddr);
+                }
+
+                action = bpf_redirect(ifindex, 0);
+                break;
+            case BPF_FIB_LKUP_RET_BLACKHOLE:    /* dest is blackholed; can be dropped */
+                action = XDP_DROP;
+                break;
+            case BPF_FIB_LKUP_RET_NOT_FWDED:    /* packet is not forwarded */
+                break;
+            default:
+                action = XDP_DROP;
+        }
+    }
+
+	return action;
+}
+
+SEC("xdp/flb4")
 int balancer_main(struct xdp_md* ctx) {
 	void *data     = (void *)(long)ctx->data;
 	void *data_end = (void *)(long)ctx->data_end;
@@ -69,7 +146,7 @@ int balancer_main(struct xdp_md* ctx) {
 	VALIDATE_HEADER(eth, data_end);
 
 	if (eth->h_proto == bpf_htons(ETH_P_IP)) {
-        return process_packet(data, data_end, sizeof(struct ethhdr));
+        return process_packet(ctx, data, data_end, sizeof(struct ethhdr));
 	}
 
 	return XDP_PASS;
--- src/core/flb4_helper.h
+++ src/core/flb4_helper.h
@@ -15,24 +15,4 @@
 		return XDP_PASS;            \
 	}
 
-static __always_inline void print_ip(const char* msg, __u32 ip) {
-    bpf_printk("%s %d.%d.%d.%d", msg ? msg : "",
-                                (ip & 0xFF000000) >> 24,
-                                (ip & 0x00FF0000) >> 16,
-                                (ip & 0x0000FF00) >> 8,
-                                (ip & 0x000000FF));
-}
-
-static __always_inline __u16 csum_fold_helper(__u32 csum) {
-    __u32 sum = csum;
-    while (sum >> 16)
-        sum = (sum & 0xffff) + (sum >> 16);
-    return ~sum;
-}
-
-static __always_inline __u16 ipv4_csum(__u32 csum, struct iphdr *iph) {
-    csum = bpf_csum_diff(0, 0, (__be32 *)iph, sizeof(*iph), csum);
-    return csum_fold_helper(csum);
-}
-
 #endif
